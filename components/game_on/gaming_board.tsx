23ec5f59380a6fbde115f27ca8dfcca7b59e96ad
Andrew Cho <cisdell@gmail.com>
2024-04-04 15:10:19 -0400

    figured out text box to type away words

--- components/game_on/gaming_board.tsx
+++ components/game_on/gaming_board.tsx
@@ -13,8 +13,18 @@ export default function GameBoard() {
     { word: "tissue", row: 3, col: 3 },
     { word: "chris", row: 5, col: 2 },
     { word: "bloomberg", row: 8, col: 3 },
+    { word: "shouldbegone", row: 9, col: 3 },
   ]);
-  const word = "apple";
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [wordsCount, setWordsCount] = useState(0);
+  const [speed, setSpeed] = useState(2000);
+  const [tryValue, setTryValue] = useState("");
+  const [life, setLife] = useState([0, 0, 0, 0, 0]);
+  const [clearedCount, setClearedCount] = useState(0);
+  const [paused, setPaused] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
+  const [audioPlaying, setAudioPlaying] = useState(false);
+
   // useEffect(() => {
   //   const interval = setInterval(() => {
   //     // Update the rows of all words
@@ -28,10 +38,34 @@ export default function GameBoard() {
 
   //   return () => clearInterval(interval); // Cleanup on unmount
   // }, []);
+  //
+
+  console.log(tryValue);
+  const setChange = (e) => {
+    const newValue = e.target.value.replace(/\s/g, "");
+    setTryValue(newValue);
+  };
+
+  //text input submit
+  const submitTry = (e) => {
+    e.preventDefault();
+    for (let i = 0; i < wordsOnScreen.length; i++) {
+      if (wordsOnScreen[i].word === tryValue) {
+        wordsOnScreen.splice(i, 1);
+        setClearedCount(clearedCount + 1);
+        setWordsOnScreen(wordsOnScreen);
+        console.log(wordsOnScreen);
+        break; // Stop loop once object is removed
+      }
+    }
+    setTryValue(""); // should clear the field after hitting return
+  };
 
   return (
-    <div className="w-[90rem] h-[50rem] border-solid m-auto border-white justify-center align-middle bg-blue-800 flex flex-col">
-      <h1>Instructions</h1>
+    <div className="w-[90rem] h-[50rem] border-solid m-auto border-white justify-center align-middle bg-blue-400 flex flex-col">
+      <h1 className="text-center text-lg">
+        Type away the words before they hit the floor!
+      </h1>
 
       <div className="relative grid grid-cols-3 grid-rows-8 gap-2">
         {grid_pos.map(({ row, col }, index) => (
@@ -40,17 +74,16 @@ export default function GameBoard() {
         {wordsOnScreen.map((w, index) => (
           <Word word={w.word} row={w.row} col={w.col} key={index} />
         ))}
-
-        {/* <div className="absolute grid-cols-subgrid row-start-8 col-start-3">
-          00
-        </div>
-        <div className="absolute grid-cols-subgrid row-start-4 col-start-2">
-          02
-        </div>
-        <div className="absolute grid-cols-subgrid row-start-2 col-start-1">
-          03
-        </div> */}
       </div>
+      <form className="ml-auto mr-auto mt-7" onSubmit={submitTry}>
+        <input
+          required
+          type="text"
+          size="40"
+          value={tryValue}
+          onChange={setChange}
+        />
+      </form>
     </div>
   );
 }
--- components/game_on/word.tsx
+++ components/game_on/word.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Word({ word, row, col }) {
   return (
     <div
-      className={`absolute grid-cols-subgrid row-start-${row} col-start-${col}`}
+      className={`absolute grid-cols-subgrid row-start-${row} col-start-${col} lm-auto rm-auto justify-center`}
     >
       {word}
     </div>
