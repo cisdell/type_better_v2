a7b676bc574081372e8ed26f875a09a91cbc9471
Andrew Cho <cisdell@gmail.com>
2024-04-03 20:45:41 -0400

    running into issues with grid display

--- components/game_on/gaming_board.tsx
+++ components/game_on/gaming_board.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 import GridPlaceholder from "./grid_placeholder";
-import { grid_pos } from "@/lib/data";
+import { grid_pos, word_bank } from "@/lib/data";
 
 export default function GameBoard() {
+  const [wordsOnScreen, setWordsOnScreen] = useState([
+    { word: "apple", row: 1, col: 1 },
+    { word: "orange", row: 1, col: 2 },
+  ]);
+  const word = "apple";
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update the rows of all words
+      setWordsOnScreen((prevWords) => {
+        return prevWords.map((word) => ({
+          ...word,
+          row: word.row + 1, // Increment the row
+        }));
+      });
+    }, 1000); // Run every second
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
     <div className="w-[90rem] h-[50rem] border-solid m-auto border-white justify-center align-middle bg-blue-800 flex flex-col">
       <h1>Instructions</h1>
-      <div className="grid grid-cols-1 gap-2">
+      <div className="grid grid-cols-3 grid-rows-8 gap-2">
         {grid_pos.map(({ row, col }, index) => (
-          <GridPlaceholder row={row} col={col} key={index} />
+          <GridPlaceholder
+            row={row}
+            col={col}
+            key={index}
+            wordsOnScreen={wordsOnScreen}
+            setWordsOnScreen={setWordsOnScreen}
+          />
         ))}
       </div>
     </div>
--- components/game_on/grid_placeholder.tsx
+++ components/game_on/grid_placeholder.tsx
@@ -1,5 +1,22 @@
+"use client";
 import React from "react";
 
-export default function GridPlaceholder() {
-  return <div className="col-span-0.5 bg-gray-200">Column 1</div>;
+export default function GridPlaceholder({
+  wordsOnScreen,
+  row,
+  col,
+  setWordsOnScreen,
+}) {
+  console.log(wordsOnScreen);
+  return (
+    <div className="col-span-0.5 bg-gray-200">
+      {wordsOnScreen.map((item, index) => {
+        if (item.row === row && item.col === col) {
+          return <div key={index}>{item.word}</div>;
+        } else {
+          return <div className="bg-slate-300">{<div></div>}</div>;
+        }
+      })}
+    </div>
+  );
 }
