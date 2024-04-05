e4584241dbad5f3697aaa4ee4f7cfcfb03484a00
Andrew Cho <cisdell@gmail.com>
2024-04-04 20:45:01 -0400

    working for now

--- components/game_on/gaming_functions.ts
+++ components/game_on/gaming_functions.ts
@@ -1,7 +1,5 @@
 'use server'
-import { word_bank } from '@/lib/data';
 
-//function to submit a try value.
 export const submitTry = (tryValue: string, wordsOnScreen: any[], setClearedCount: Function, setWordsOnScreen: Function): void => {
   for (let i = 0; i < wordsOnScreen.length; i++) {
     if (wordsOnScreen[ i ].word === tryValue) {
