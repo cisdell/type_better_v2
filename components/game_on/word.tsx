434c2ece0d2b71c9976ea94c70f3b3c5127af80c
Andrew Cho <cisdell@gmail.com>
2024-04-07 21:32:00 -0400

    type issues all fixed

--- components/game_on/word.tsx
+++ components/game_on/word.tsx
@@ -1,21 +1,5 @@
 import React, { useEffect } from "react";
-
-// const test1 = 'absolute row-start-1 col-start-1',;
-// const test2 = 'absolute row-start-1 col-start-2',;
-// const test3 = 'absolute row-start-1 col-start-3',;
-// const row1 = "absolute row-start-1";
-// const row2 = "absolute row-start-2";
-// const row3 = "absolute row-start-3";
-// const row4 = "absolute row-start-4";
-// const row5 = "absolute row-start-5";
-// const row6 = "absolute row-start-6";
-// const row7 = "absolute row-start-7";
-// const row8 = "absolute row-start-8";
-// const row9 = "absolute row-start-9";
-
-// const col1 = "col-start-1";
-// const col2 = "col-start-2";
-// const col3 = "col-start-3";
+import { WordType } from "@/util/types";
 
 export default function Word({
   word,
@@ -37,17 +21,17 @@ export default function Word({
 
   useEffect(() => {
     if (expired(row)) {
-      if (life === 0) {
+      if (life.length === 0) {
         setGameOver(true);
       }
       life.pop();
       setLife(life);
       console.log("expired" + expired(row));
+      //filter out the missed word from the object
       for (let i = 0; i < wordsOnScreen.length; i++) {
         if (wordsOnScreen[i].word === word) {
           wordsOnScreen.splice(i, 1);
           setWordsOnScreen(wordsOnScreen);
-          console.log(wordsOnScreen);
           break; // Stop loop once object is removed
         }
       }
@@ -64,13 +48,13 @@ export default function Word({
   );
 }
 
-type WordType = {
-  word: string;
-  row: number;
-  col: number;
-  life: number;
-  setLife: Function;
-  setGameOver: Function;
-  wordsOnScreen: any;
-  setWordsOnScreen: Function;
-};
+// type WordType = {
+//   word: string;
+//   row: number;
+//   col: number;
+//   life: number;
+//   setLife: Function;
+//   setGameOver: Function;
+//   wordsOnScreen: any;
+//   setWordsOnScreen: Function;
+// };
