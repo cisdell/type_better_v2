b3977007e144aeef7383ac4ba5e89e292337673e
Andrew Cho <cisdell@gmail.com>
2024-04-15 21:17:01 -0400

    instead of using onclick i am using a form

--- components/game_off/demo.tsx
+++ components/game_off/demo.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function Demo({ setDemoOn, setCountdownOn }: any) {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setDemoOn(false);
+    setCountdownOn(true);
+  };
   return (
     <div className="absolute inset-0 flex flex-col z-10 justify-center items-center bg-slate-600 ml-[20%] mr-[20%] mt-[10%] mb-[10%] rounded-3xl z-10">
-      <h1>demo</h1>
-
+      <h1></h1>
       <video
         width="640"
         height="480"
@@ -15,15 +19,15 @@ export default function Demo({ setDemoOn, setCountdownOn }: any) {
       >
         <source src="/images/demo_video.mp4" type="video/mp4" />
       </video>
-      <button
-        className="h-8 w-20 bg-blue-500 text-white rounded-lg"
-        onClick={() => {
-          setDemoOn(false);
-          setCountdownOn(true);
-        }}
-      >
-        Close
-      </button>
+      <form onSubmit={handleSubmit}>
+        <button
+          className="h-8 w-20 bg-blue-500 text-white rounded-lg"
+          type="submit"
+          autoFocus
+        >
+          Close
+        </button>
+      </form>
     </div>
   );
 }
--- components/game_off/level1_modal.tsx
+++ components/game_off/level1_modal.tsx
@@ -5,6 +5,12 @@ export default function LevelOneModal({
   setPaused,
   setCountdownOn,
 }: any) {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setModalOn(false);
+    setPaused(false);
+    setCountdownOn(true);
+  };
   return (
     <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-800 text-white ml-[20%] mr-[20%] mt-[5%] mb-[5%] rounded-3xl z-20">
       <h2>
@@ -17,16 +23,15 @@ export default function LevelOneModal({
         height={600}
         alt="Level1 clear"
       />
-      <button
-        className="h-8 w-20 bg-blue-500 text-white rounded-lg"
-        onClick={() => {
-          setModalOn(false);
-          setPaused(false);
-          setCountdownOn(true);
-        }}
-      >
-        Close
-      </button>
+      <form onSubmit={handleSubmit}>
+        <button
+          className="h-8 w-20 bg-blue-500 text-white rounded-lg"
+          type="submit"
+          autoFocus
+        >
+          Close
+        </button>
+      </form>
     </div>
   );
 }
