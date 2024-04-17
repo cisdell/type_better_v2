d6081d05686ba1b901839c511ae433f2e016f24c
Andrew Cho <cisdell@gmail.com>
2024-04-17 15:10:53 -0400

    button styled

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,25 +3,31 @@ import React from "react";
 export default function NavBar() {
   return (
     // <nav className="h-[3rem] w-auto bg-gray-400 flex justify-end justify-around text-xl">
-    <nav className="h-[3rem] w-auto bg-gray-400 flex justify-between">
-      <div>
-        <span className="w-[20%] ml-8">Welcome USERNsdfsdfsdfAME</span>
+    <nav className="h-14 w-auto bg-black flex items-center">
+      <div className="w-[50%]">
+        <span className="w-[30%] ml-10 text-white">
+          Welcome USERNsdfsdfsdfAME
+        </span>
       </div>
-      <div className="r-0">
-        <a className="" href="">
+
+      <div className="w-[50%] flex justify-end gap-4 pr-5">
+        <a
+          className="h-10 w-28 text-center border border-yellow-600 rounded-lg pt-2 text-black bg-yellow-600 hover:cursor-pointer"
+          href=""
+        >
           Home
         </a>
-        <a className="" href="">
+        <a className="h-10 w-28 text-center border border-yellow-600 rounded-lg pt-2 text-black bg-yellow-600 hover:cursor-pointer">
           Login
         </a>
-        <a className="" href="">
+        <a className="h-10 w-28 text-center border border-yellow-600 rounded-lg pt-2 text-black bg-yellow-600 hover:cursor-pointer">
           Leaderboard
         </a>
-        <a className="" href="">
+        <a className="h-10 w-28 text-center border border-yellow-600 rounded-lg pt-2 text-black bg-yellow-600 hover:cursor-pointer">
           Add Request
         </a>
         <a
-          className="mr-7"
+          className="h-10 w-28 text-center border border-yellow-600 rounded-lg pt-2 text-black bg-yellow-600 hover:cursor-pointer"
           href="https://hireandrewcho.vercel.app/"
           target="_blank"
         >
